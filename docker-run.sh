8baccfaa6c742f8655c69370828124502723f943
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-02-23 23:50:33 -0800

    Add docker run file to deploy ByteTrack with one script

---
+++ docker-run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+name=$1
+input=$2
+docker_image=bytetrack:manual
+prefix=river
+fps=15
+bytetrack_home=/home/salmonjetson/ByteTrack
+
+# Run ByteTrack on input outputting to the YOLOX_outputs folder into the $prefix folder
+sudo docker run -it --rm --runtime nvidia \
+    -v ${bytetrack_home}/pretrained:/ByteTrack/pretrained \
+    -v ${bytetrack_home}/datasets:/ByteTrack/datasets \
+    -v ${bytetrack_home}/YOLOX_outputs:/ByteTrack/YOLOX_outputs \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix:rw \
+    -w /ByteTrack/YOLOX_outputs \
+    --device /dev/video0:/dev/video0:mwr \
+    --net=host \
+    --name "$name" \
+    -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    -e DISPLAY=$DISPLAY \
+    --privileged \
+    $docker_image \
+    ../deploy/TensorRT/cpp/build/bytetrack ../deploy/TensorRT/cpp/model_trt.engine \
+    -i "${input}" $prefix $fps
