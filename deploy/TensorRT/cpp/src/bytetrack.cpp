11ab2545557e109ff86994bb3a18d1462e7ba4f4
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-04-24 09:39:55 -0700

    Use gstreamer with GPU for encoding

--- deploy/TensorRT/cpp/src/bytetrack.cpp
+++ deploy/TensorRT/cpp/src/bytetrack.cpp
@@ -517,7 +517,10 @@ int main(int argc, char** argv) {
         const std::string save_path = (fs::path(save_folder) / fs::path(timestamp + "_" + output_suffix + ".mp4")).string();
 
         cout << "video save_path is " << save_path << endl;
-        VideoWriter writer(save_path, VideoWriter::fourcc('m', 'p', '4', 'v'), fps, Size(img_w, img_h));
+
+        const auto gst_writer_str = "appsrc ! video/x-raw,format=BGR,width={"+to_string(img_w)+"},height={"+to_string(img_h)+"},framerate={"+to_string(fps)+"}/1 ! queue ! videoconvert ! video/x-raw,format=BGRx ! nvvidconv ! nvv4l2h264enc insert-vui=1 ! h264parse ! qtmux ! filesink location=" + save_path;
+        //VideoWriter writer(save_path, VideoWriter::fourcc('m', 'p', '4', 'v'), fps, Size(img_w, img_h));
+        VideoWriter writer(gst_writer_str, CAP_GSTREAMER, 0, fps, Size(img_w, img_h), true);
 
 
         std::string counts_filename = (fs::path(save_folder) / fs::path(timestamp + "_" + output_suffix + "_counts.csv")).string();
