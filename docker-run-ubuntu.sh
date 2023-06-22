1aead9690026fff8f0a83bf7af9b89f5a6343be0
KamiCreed <9517086+KamiCreed@users.noreply.github.com>
2023-06-22 15:55:54 -0700

    Add dev docker run

---
+++ docker-run-ubuntu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+name=$1
+
+# Constants
+docker_image=bytetrack
+bytetrack_home=~/ByteTrack
+workspace=/workspace/mot_dev
+
+docker run -it --rm --runtime nvidia \
+    -v "${bytetrack_home}:${workspace}" \
+    -w "$workspace" \
+    --net host --name bytetrack --privileged \
+    -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    $docker_image
